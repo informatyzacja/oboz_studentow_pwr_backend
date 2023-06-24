fa06e90ec250f3b7215d67468af7c9348fbd2281
Marvin Ruciński <marvin@prasa-polska.com>
2023-06-24 18:58:52 +0200

    added UserManagere (to create superusers)

--- obozstudentow/models/user.py
+++ obozstudentow/models/user.py
@@ -4,6 +4,25 @@ from django.db import models
 from django_resized import ResizedImageField
 from .models import Bus
 
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_superuser(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email")
+        if not password:
+            raise ValueError("User must have a password")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.admin = True
+        user.staff = True
+        user.active = True
+        user.save(using=self._db)
+        return user
+
 class User(AbstractUser):
     username=None
     email = models.EmailField(unique=True)
@@ -18,6 +37,7 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    objects = UserManager()
 
     def __str__(self):
         return self.first_name + " " + self.last_name + " " + self.title
