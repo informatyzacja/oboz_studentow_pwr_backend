210f595c41e39af44891d93d8e6e6db2b417bbe7
Marvin Ruciński <marvin@prasa-polska.com>
2023-06-27 18:52:31 +0200

    added rate limit

--- obozstudentowProject/settings.py
+++ obozstudentowProject/settings.py
@@ -183,7 +183,15 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '30/minute'
+    }
 }
 
 if DEBUG:
