f45f42122b94a9af191dd0b55c11b0b979f41992
Marvin Ruciński <marvin@prasa-polska.com>
2023-08-17 16:43:50 +0200

    allowed null values on user model

--- obozstudentow/models/user.py
+++ obozstudentow/models/user.py
@@ -26,12 +26,12 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     username=None
     email = models.EmailField(unique=True)
-    phoneNumber = models.CharField(max_length=9, blank=True)
-    bandId = models.CharField(max_length=10, blank=True)
-    houseNumber = models.CharField(max_length=10, blank=True)
-    photo = ResizedImageField(upload_to='users', blank=True) 
-    title = models.CharField(max_length=100, blank=True) # e.g. "Koordynator"
-    diet = models.CharField(max_length=100, blank=True) # e.g. "wegetariańska"
+    phoneNumber = models.CharField(max_length=9, blank=True, null=True)
+    bandId = models.CharField(max_length=10, blank=True, null=True)
+    houseNumber = models.CharField(max_length=10, blank=True, null=True)
+    photo = ResizedImageField(upload_to='users', blank=True, null=True) 
+    title = models.CharField(max_length=100, blank=True, null=True) # e.g. "Koordynator"
+    diet = models.CharField(max_length=100, blank=True, null=True) # e.g. "wegetariańska"
     bus = models.ForeignKey(Bus, on_delete=models.SET_NULL, null=True, blank=True)
     
     USERNAME_FIELD = 'email'
