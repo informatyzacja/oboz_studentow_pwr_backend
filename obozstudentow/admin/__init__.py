5ba2725b7b701298a25ca688106e7f37f4ab8df0
Marvin Ruciński <marvin@prasa-polska.com>
2023-08-21 17:32:26 +0200

    added admin action to activete all users

--- obozstudentow/admin/__init__.py
+++ obozstudentow/admin/__init__.py
@@ -2,15 +2,14 @@ from .group import *
 from .meals import *
 from .people import *
 from .workshop import *
+from .user import *
 
 from import_export.admin import ImportExportModelAdmin
 
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.admin import UserAdmin
-from ..models import Link, FAQ, ScheduleItem, User, Icons, UserFCMToken
+from ..models import Link, FAQ, ScheduleItem, User, Icons
 
-from django.contrib.auth.models import Group
 
 admin.site.site_header = "Panel administracyjny Obozu Studentów PWR 2023"
 admin.site.site_title = "Panel administracyjny Obozu Studentów PWR 2023"
@@ -35,204 +34,10 @@ class IconsAdmin(admin.ModelAdmin):
     list_display = ('name', 'icon')
     search_fields = ('name', 'icon')
 
-class GroupMemberInlineAdmin(admin.TabularInline):
-    model = GroupMember
-    extra = 1
-
-from django.contrib.auth.forms import BaseUserCreationForm
-from django.core.exceptions import ValidationError
-
-class UserCreationFormEmail(BaseUserCreationForm):
-    password1 = None
-    password2 = None
-
-    def save(self, commit=True):
-        user = super(BaseUserCreationForm, self).save(commit=False)
-        if commit:
-            user.save()
-            if hasattr(self, "save_m2m"):
-                self.save_m2m()
-        return user
-
-    def clean_email(self):
-        """Reject usernames that differ only in case."""
-        email = self.cleaned_data.get("email")
-        if (
-            email
-            and self._meta.model.objects.filter(email__iexact=email).exists()
-        ):
-            self._update_errors(
-                ValidationError(
-                    {
-                        "email": self.instance.unique_error_message(
-                            self._meta.model, ["email"]
-                        )
-                    }
-                )
-            )
-        else:
-            return email
-        
-
 admin.site.register(Link, LinkAdmin)
 admin.site.register(FAQ, FAQAdmin)
 admin.site.register(ScheduleItem, ScheduleItemAdmin)
         
-class UserFCMTokenInline(admin.TabularInline):
-    model = UserFCMToken
-    extra = 0
-    classes = ['collapse']
-
-class Participant(User):
-    class Meta:
-        proxy = True
-        verbose_name = 'Uczestnik'
-        verbose_name_plural = 'Uczestnicy'
-
-    
-
-class ParticipantAdmin(ImportExportModelAdmin, UserAdmin):
-    def get_queryset(self, request):
-        return self.model.objects.filter(groups=None)
-
-    def frakcja(self, user):
-        groups = []
-        for group in GroupMember.objects.filter(user=user, group__type=GroupType.objects.get(name='Frakcja')):
-            groups.append(group.group.name)
-        return ' '.join(groups)
-    frakcja.short_description = 'Frakcja'
-
-    list_display = ('id', "email", 'first_name', 'last_name', 'bandId', 'frakcja', 'is_active')
-    search_fields = ('first_name', "email", 'last_name', 'title', 'phoneNumber', 'bandId', 'houseNumber', 'title')
-
-    list_filter = ('bus', "is_active")
-    ordering = ("last_name",'first_name')
-
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("email", "first_name", "last_name"),
-            },
-        ),
-    )
-    add_form = UserCreationFormEmail
-
-
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name")}),
-        ("Dodatkowe informacje", {"fields": ("phoneNumber", "bandId", "houseNumber", "photo", "title", "diet", "bus", 'birthDate', 'freenow_code')}),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                ),
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
-    inlines = [GroupMemberInlineAdmin, GroupWardenInline, UserFCMTokenInline]
-
-admin.site.register(Participant, ParticipantAdmin)
-
-
-class Kadra(User):
-    class Meta:
-        proxy = True
-        verbose_name = 'Kadrowicz'
-        verbose_name_plural = 'Kadra'
-
-class KadraAdmin(ParticipantAdmin):
-    def get_queryset(self, request):
-        return self.model.objects.filter(groups__name__in=['Kadra','Bajer'])
-    
-    list_display = ('id', "email", 'first_name', 'last_name', 'bandId', 'frakcja', 'title', 'is_active', 'has_image')
-    
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name")}),
-        ("Dodatkowe informacje", {"fields": ("phoneNumber", "bandId", "houseNumber", "photo", "title", "diet", "bus", 'birthDate', 'freenow_code')}),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "groups",
-                ),
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
-
-    list_filter = ('groups', 'bus', "is_active")
-    
-admin.site.register(Kadra, KadraAdmin)
-   
-class CustomUserAdmin(ImportExportModelAdmin, UserAdmin):
-    def frakcja(self, user):
-        groups = []
-        for group in GroupMember.objects.filter(user=user, group__type=GroupType.objects.get(name='Frakcja')):
-            groups.append(group.group.name)
-        return ' '.join(groups)
-    frakcja.short_description = 'Frakcja'
-
-    list_display = ('id', "email", 'first_name', 'last_name', 'bandId', 'frakcja', 'title', 'is_active', "is_staff", 'has_image')
-    search_fields = ('first_name', "email", 'last_name', 'title', 'phoneNumber', 'bandId', 'houseNumber', 'title')
-
-    list_filter = ("groups", 'bus', "is_staff", "is_active")
-    ordering = ("last_name",'first_name')
-
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("email", "first_name", "last_name"),
-            },
-        ),
-    )
-    add_form = UserCreationFormEmail
-
-
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name")}),
-        ("Dodatkowe informacje", {"fields": ("phoneNumber", "bandId", "houseNumber", "photo", "title", "diet", "bus", 'birthDate', 'freenow_code')}),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                ),
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
-    inlines = [GroupMemberInlineAdmin, GroupWardenInline, UserFCMTokenInline]
-
-admin.site.register(User, CustomUserAdmin)
-
-
-class Sztab(User):
-    class Meta:
-        proxy = True
-        verbose_name = 'Sztab'
-        verbose_name_plural = 'Sztab'
-
-class SztabAdmin(CustomUserAdmin):
-    def get_queryset(self, request):
-        return self.model.objects.filter(groups__name__in=['Sztab'])
-    
-admin.site.register(Sztab, SztabAdmin)
-
-
 from ..models import Announcement
 
 @admin.register(Announcement)
