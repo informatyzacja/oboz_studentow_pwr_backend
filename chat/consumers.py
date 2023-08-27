faecce4d8b27a1c6163e93ea589fd0c3ff7e54d6
Marvin Ruciński <marvin@prasa-polska.com>
2023-08-27 12:46:34 +0200

    added prints for info

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,18 +3,23 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
 	async def connect(self):
+		print("connected")
 		self.roomGroupName = "group_chat_gfg"
 		await self.channel_layer.group_add(
 			self.roomGroupName ,
 			self.channel_name
 		)
 		await self.accept()
+
 	async def disconnect(self , close_code):
+		print("disconnected")
 		await self.channel_layer.group_discard(
 			self.roomGroupName ,
 			self.channel_layer
 		)
+
 	async def receive(self, text_data):
+		print("received", text_data)
 		text_data_json = json.loads(text_data)
 		message = text_data_json["message"]
 		username = text_data_json["username"]
@@ -24,7 +29,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
 				"message" : message ,
 				"username" : username ,
 			})
-	async def sendMessage(self , event) :
+		
+	async def sendMessage(self , event):
+		print("send message", event)
 		message = event["message"]
 		username = event["username"]
 		await self.send(text_data = json.dumps({"message":message ,"username":username}))
