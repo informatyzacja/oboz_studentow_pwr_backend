b4565a5aa6df6d2a1c1c664e626776552570fc3e
Marvin Ruciński <marvin@prasa-polska.com>
2024-08-30 19:03:31 +0200

    dodanie osób z dojazdem własnym do sprawdziania obecności

--- obozstudentow/api/staff/buses.py
+++ obozstudentow/api/staff/buses.py
@@ -15,11 +15,28 @@ def get_buses(request):
         return Response({'success': False, 'error': 'Sprawdzanie obecności w busach nie jest aktywowane'})
     bus_presence_to = bus_presence_type == 'to'
 
-    return Response(Bus.objects.all().annotate(
-        users_count=Count('user', filter=Q(user__bus_info__in=(User.BusInfoChoices.BOTH, User.BusInfoChoices.TO))) if bus_presence_to else Count('user', filter=Q(user__bus_info__in=(User.BusInfoChoices.BOTH, User.BusInfoChoices.RETURN))),
-        present_users_count=Count('user', filter=Q(user__bus_presence=True, user__bus_info__in=[User.BusInfoChoices.BOTH, User.BusInfoChoices.TO])) if bus_presence_to else Count('user', filter=Q(user__bus_presence_return=True, user__bus_info__in=[User.BusInfoChoices.BOTH,  User.BusInfoChoices.RETURN])),
-        opaski_count=Count('user', filter=Q(user__bandId__isnull=False, user__bus_info__in=[User.BusInfoChoices.BOTH, User.BusInfoChoices.TO if bus_presence_to else User.BusInfoChoices.RETURN]))
-    ).order_by('description').values('id', 'description', 'users_count', 'present_users_count', 'opaski_count'))
+    users_count = User.objects.filter(bus__isnull=True).count()
+    present_users_count = User.objects.filter(Q(bus_presence=True) if bus_presence_to else Q(bus_presence_return=True), bus__isnull=True).count()
+    opaski_count = User.objects.filter(bus__isnull=True, bandId__isnull=False).count()
+
+    return Response(
+        list(
+            Bus.objects.all().annotate(
+
+                users_count=Count('user', filter=Q(user__bus_info__in=(User.BusInfoChoices.BOTH, User.BusInfoChoices.TO))) if bus_presence_to else Count('user', filter=Q(user__bus_info__in=(User.BusInfoChoices.BOTH, User.BusInfoChoices.RETURN))),
+
+                present_users_count=Count('user', filter=Q(user__bus_presence=True, user__bus_info__in=[User.BusInfoChoices.BOTH, User.BusInfoChoices.TO])) if bus_presence_to else Count('user', filter=Q(user__bus_presence_return=True, user__bus_info__in=[User.BusInfoChoices.BOTH,  User.BusInfoChoices.RETURN])),
+
+                opaski_count=Count('user', filter=Q(user__bandId__isnull=False, user__bus_info__in=[User.BusInfoChoices.BOTH, User.BusInfoChoices.TO if bus_presence_to else User.BusInfoChoices.RETURN]))
+
+            ).order_by('description').values('id', 'description', 'users_count', 'present_users_count', 'opaski_count')
+         ) + [{
+        'id': 0,
+        'description': 'Dojazd własny',
+        'users_count': users_count,
+        'present_users_count': present_users_count,
+        'opaski_count': opaski_count
+    }])
 
 @api_view(['GET'])
 @permission_required('obozstudentow.can_check_bus_presence')
@@ -27,18 +44,25 @@ def get_bus_users(request):
     if 'bus_id' not in request.GET:
         return Response({'success': False, 'error': 'Nie podano ID autobusu'})
 
-    if not Bus.objects.filter(id=request.GET['bus_id']).exists():
+    if request.GET['bus_id'] != '0' and not Bus.objects.filter(id=request.GET['bus_id']).exists():
         return Response({'success': False, 'error': 'Autobus nie istnieje'})
-
-    bus = Bus.objects.get(id=request.GET['bus_id'])
 
     bus_presence_type = Setting.objects.get(name="bus_presence").value.lower()
     bus_presence_to = bus_presence_type == 'to'
     presence_type  = 'bus_presence' if bus_presence_to else 'bus_presence_return'
 
-    return Response(User.objects.filter(bus=bus, bus_info__in=[User.BusInfoChoices.BOTH, User.BusInfoChoices.TO if bus_presence_to else User.BusInfoChoices.RETURN]).annotate(
-        bandId_isnull=Count('bandId')
-    ).order_by(presence_type, 'bandId_isnull', 'last_name', 'first_name').values('id', 'first_name', 'last_name', 'phoneNumber', 'bandId', 'bus_info',  presence = F(presence_type)))
+    if Bus.objects.filter(id=request.GET['bus_id']).exists():
+        bus = Bus.objects.get(id=request.GET['bus_id'])
+
+        return Response(User.objects.filter(bus=bus, bus_info__in=[User.BusInfoChoices.BOTH, User.BusInfoChoices.TO if bus_presence_to else User.BusInfoChoices.RETURN]).annotate(
+            bandId_isnull=Count('bandId')
+        ).order_by(presence_type, 'bandId_isnull', 'last_name', 'first_name').values('id', 'first_name', 'last_name', 'phoneNumber', 'bandId', 'bus_info',  presence = F(presence_type)))
+    
+    else:
+        return Response(User.objects.filter(bus__isnull=True).annotate(
+            bandId_isnull=Count('bandId')
+        ).order_by(presence_type, 'bandId_isnull', 'last_name', 'first_name').values('id', 'first_name', 'last_name', 'phoneNumber', 'bandId', 'bus_info',  presence = F(presence_type)))
+
 
 @api_view(['PUT'])
 @permission_required('obozstudentow.can_check_bus_presence')
