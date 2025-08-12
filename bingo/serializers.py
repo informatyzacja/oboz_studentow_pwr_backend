32c46a4b43157e5dbfc4fbe91b78d88209f78f3c
Torgard-Mirdas <160980137+Torgard-Mirdas@users.noreply.github.com>
2025-08-12 10:52:08 +0200

    Update serializers.py

--- bingo/serializers.py
+++ bingo/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import BingoUserInstance, BingoUserTask, BingoTaskTemplate
-
+from .utils import check_bingo_win
 
 class BingoTaskTemplateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,13 +10,13 @@ class BingoTaskTemplateSerializer(serializers.ModelSerializer):
 
 class BingoUserTaskSerializer(serializers.ModelSerializer):
     task = BingoTaskTemplateSerializer()
-    photo_url = serializers.SerializerMethodField()
+    photo_proof = serializers.SerializerMethodField()
 
     class Meta:
         model = BingoUserTask
         fields = "__all__"
 
-    def get_photo_url(self, obj):
+    def get_photo_proof(self, obj):
         if obj.photo_proof:
             request = self.context.get("request")
             return request.build_absolute_uri(obj.photo_proof.url)
@@ -24,8 +24,12 @@ class BingoUserTaskSerializer(serializers.ModelSerializer):
 
 
 class BingoUserInstanceSerializer(serializers.ModelSerializer):
-    tasks = BingoUserTaskSerializer(many=True, read_only=True)
+    can_be_submitted = serializers.SerializerMethodField()
+
+    def get_can_be_submitted(self, obj):
+        # Bingo można wysłać jeśli jest wygrane i jeszcze nie zostało zatwierdzone (completed_at == None)
+        return check_bingo_win(obj) and (obj.completed_at is None)
 
     class Meta:
         model = BingoUserInstance
-        fields = "__all__"
+        fields = ('id', 'can_be_submitted', ...)
